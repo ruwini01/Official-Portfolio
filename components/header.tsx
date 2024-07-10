6d73318a99dc41ce647013caf40e3cbe6da4bdaa
ruwini01 <12345tharanga12345@gmail.com>
2024-07-10 18:44:11 +0530

    Project Initialization

---
+++ components/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return <header className='z-[999] relative'>
+    <div className= 'fixed top-0 left-0 h-[4.5rem] w-full bg-black bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:h-[4.25rem]'>
+
+    </div>
+
+  </header>;
+}
