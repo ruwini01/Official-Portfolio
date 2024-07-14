00d303de17e6eaa40bf36887ea3295b969fa700d
ruwini01 <12345tharanga12345@gmail.com>
2024-07-15 00:34:41 +0530

    about changes

--- components/about.tsx
+++ components/about.tsx
@@ -10,17 +10,16 @@ export default function About() {
   return (
     <div className="flex flex-col items-center justify-center"> 
       <motion.section
-        ref = {ref}
-        className="max-w-[22rem] lg:max-w-[60rem] text-center leading-8 sm:mb-40 mt-32 lg:mt-0 scroll-mt-40 lg:scroll-mt-60 "
+        ref={ref}
+        className="max-w-[22rem] lg:max-w-[60rem] text-center leading-8 sm:mb-40 mt-32 lg:mt-0 scroll-mt-40 lg:scroll-mt-60"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.175 }}
         id="about"
-        
       >
         <SectionHeading>About Me</SectionHeading>
         <p className="mb-3">
-          I am a dedicated <span className="text-red-500 font-medium">UX/UI designer</span> and <span className="text-red-500 font-medium">developer</span> with a passion for creating seamless and engaging user experiences. With a strong background in both design and development, I excel at transforming complex problems into intuitive, user-friendly solutions. My goal is to continuously innovate and deliver impactful digital products that meet users' needs and exceed their expectations.
+          I am a dedicated <span className="text-red-500 font-medium">UX/UI designer</span> and <span className="text-red-500 font-medium">developer</span> with a passion for creating seamless and engaging user experiences. With a strong background in both design and development, I excel at transforming complex problems into intuitive, user-friendly solutions. My goal is to continuously innovate and deliver impactful digital products that meet users&apos; needs and exceed their expectations.
         </p>
 
         <div className="flex justify-center mt-8">
@@ -35,7 +34,7 @@ export default function About() {
     </div>
   );
 }
+
 function setActiveSection(arg0: string) {
   throw new Error('Function not implemented.');
 }
-
